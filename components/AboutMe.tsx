5ff52b4b0e867a3aee674d6bc6ee787dea052b6d
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-09 15:18:56 +0800

    services section design complete

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,12 +1,12 @@
 type Props = {};
 function AboutMe({}: Props) {
   return (
-    <div className=" py-20 p-8 bg-[#202020] text-white">
-        <div className=" flex flex-col  items-center space-y-4">
+    <div className="  py-20 p-8 bg-[#202020] text-white">
+        <div className=" lg:w-[1200px] m-auto flex flex-col  items-center space-y-4">
 
-      <h3 className="  text-bluestyle ">About Me</h3>
-      <h2 className=" text-3xl">Welcome To My Site</h2>
-      <p className=" text-sm text-center opacity-60 font-light">
+      <h3 className="  text-bluestyle  ">About Me</h3>
+      <h2 className=" text-3xl md:text-4xl">Welcome To My Site</h2>
+      <p className=" lg:w-[700px] md:text-base text-sm text-center opacity-60 font-light">
         I am a website designer from London, with a strong focus in UI/UX
         design. I love to get new experiences and always learn from my
         surroundings. I've done more than 50 projects. You can check it through
---
+++ components/Services.tsx
@@ -0,0 +1,52 @@
+import { GlobeAsiaAustraliaIcon, LightBulbIcon, PaperAirplaneIcon, TagIcon } from "@heroicons/react/24/outline";
+
+type Props = {};
+function Services({}: Props) {
+  return (
+    <div className=" p-4">
+      <div className=" flex flex-col  items-center">
+        <h3 className=" text-bluestyle">My Services</h3>
+        <h2 className=" text-3xl md:text-4xl">I'm Available For Hire</h2>
+      </div>
+
+      <div className=" flex flex-col items-center md:flex-row md:flex-wrap md:justify-center ">
+
+        
+      <div className=" cursor-pointer group m-4 hover:bg-bluestyle transition-all ease-linear duration-200 space-y-3 rounded-lg p-4 flex flex-col items-center justify-center text-center h-[300px]  border w-[300px]">
+        <LightBulbIcon className=" group-hover:text-white  transition-all ease-linear duration-200  text-bluestyle w-[60px]" />
+        <h4 className=" group-hover:text-white transition-all ease-linear duration-200 font-semibold text-xl">Design</h4>
+        <p className=" group-hover:text-white transition-all ease-linear duration-200">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem.
+        </p>
+      </div>
+      <div className=" cursor-pointer group m-4 hover:bg-bluestyle transition-all ease-linear duration-200 space-y-3 rounded-lg p-4 flex flex-col items-center justify-center text-center h-[300px]  border w-[300px]">
+        <GlobeAsiaAustraliaIcon className=" group-hover:text-white  transition-all ease-linear duration-200  text-bluestyle w-[60px]" />
+        <h4 className=" group-hover:text-white transition-all ease-linear duration-200 font-semibold text-xl">SEO</h4>
+        <p className=" group-hover:text-white transition-all ease-linear duration-200">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem.
+        </p>
+      </div>
+      <div className=" cursor-pointer group m-4 hover:bg-bluestyle transition-all ease-linear duration-200 space-y-3 rounded-lg p-4 flex flex-col items-center justify-center text-center h-[300px]  border w-[300px]">
+        <TagIcon className=" group-hover:text-white  transition-all ease-linear duration-200  text-bluestyle w-[60px]" />
+        <h4 className=" group-hover:text-white transition-all ease-linear duration-200 font-semibold text-xl">Digital Marketing</h4>
+        <p className=" group-hover:text-white transition-all ease-linear duration-200">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem.
+        </p>
+      </div>
+      <div className=" cursor-pointer group m-4 hover:bg-bluestyle transition-all ease-linear duration-200 space-y-3 rounded-lg p-4 flex flex-col items-center justify-center text-center h-[300px]  border w-[300px]">
+        <PaperAirplaneIcon className=" group-hover:text-white  transition-all ease-linear duration-200  text-bluestyle w-[60px]" />
+        <h4 className=" group-hover:text-white transition-all ease-linear duration-200 font-semibold text-xl">Migration</h4>
+        <p className=" group-hover:text-white transition-all ease-linear duration-200">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem.
+        </p>
+      </div>
+      </div>
+
+    </div>
+  );
+}
+export default Services;
