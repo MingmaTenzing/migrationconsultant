215e46f0a0ec138fa7e5258ded1d5496b6c77262
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-09 14:35:19 +0800

    about section underwork

---
+++ components/AboutMe.tsx
@@ -0,0 +1,20 @@
+type Props = {};
+function AboutMe({}: Props) {
+  return (
+    <div className=" py-20 p-8 bg-[#202020] text-white">
+        <div className=" flex flex-col  items-center space-y-4">
+
+      <h3 className="  text-bluestyle ">About Me</h3>
+      <h2 className=" text-3xl">Welcome To My Site</h2>
+      <p className=" text-sm text-center opacity-60 font-light">
+        I am a website designer from London, with a strong focus in UI/UX
+        design. I love to get new experiences and always learn from my
+        surroundings. I've done more than 50 projects. You can check it through
+        portfolio section on this website. I looking forward to any
+        opportunities and challenges.
+      </p>
+        </div>
+    </div>
+  );
+}
+export default AboutMe;
---
+++ components/Hero.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image"
+import { Typewriter } from "react-simple-typewriter";
+import FacebookIcon from "../assests/Facebook.png";
+import InstagramIcon from "../assests/Instagram.png";
+import LinkedInIcon from "../assests/LinkedIn.png"
+import profilePicture from "../assests/profile.png"
+
+type Props = {}
+function Hero({}: Props) {
+  return (
+    <main className=" lg:w-[1200px] m-auto lg:flex  lg:items-center pt-[130px] md:px-10 md:pt-[160px] lg:py-0 lg:pt-20 px-4 bg-[#f6f6f6]">
+    <div className=" space-y-3 md:space-y-6">
+      <h1 className=" font-semibold md:text-xl "> Carlos Castro</h1>
+      <h2 className=" font-bold text-4xl md:text-5xl">
+        I'm a
+        <span className="  text-bluestyle ml-3">
+          <Typewriter
+            words={["Migration Consultant", "Executive Manager", "S. Marketing Trainer"]}
+            loop={true}
+            typeSpeed={70}
+            cursor
+            cursorColor="#1e88e9"
+          />
+        </span>
+      </h2>
+      <p className=" md:text-xl font-light text-gray-500">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam.
+      </p>
+
+      <div className=" flex space-x-3 md:space-x-4 ">
+        <Image src={LinkedInIcon} alt="LinkedIn Logo " width={100} height={100} className="w-10     hover:scale-110 transition-all ease-linear duration-150" />
+        <Image src={InstagramIcon} alt="Instagram Logo " width={100} height={100} className="w-10     hover:scale-110 transition-all ease-linear duration-150" />
+        <Image src={FacebookIcon} alt="Facebook Logo " width={100} height={100} className="w-10     hover:scale-110 transition-all ease-linear duration-150" />
+
+      </div>
+    </div>
+
+    <div>
+
+      <Image src={profilePicture} alt="Carlos Castro Image" width={697} height={700}  className=" lg:w-[800px]" />
+    </div>
+
+
+  </main>
+  )
+}
+export default Hero
