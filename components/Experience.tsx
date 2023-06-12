c5269df7e3a740800df513410cdbc10233e50099
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-12 22:38:07 +0800

    working on experience section

---
+++ components/Experience.tsx
@@ -0,0 +1,79 @@
+type Props = {};
+function Experience({}: Props) {
+  return (
+    <div className=" p-4">
+      <div className="  ">
+        <h2 className="  mb-6 text-2xl font-semibold">Experiences</h2>
+
+        <div className=" space-y-8">
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              DEC 2021 - PRESENT
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Migration Consultant -{" "}
+              <span className=" font-light">Sydney Visa</span>
+            </h3>
+            <p>
+              Initially consulting and marketing with potential clients for
+              various service needs from all around the world. Online. Preparing
+              and editing all visa-related submissions and applications (e.g.,
+              visa applications (188, 482, 190, 500), skills assessments,
+              nomination applications, etc).
+            </p>
+          </div>
+
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              JAN 2021 - MAY 2022
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Executive Manager - <span className=" font-light">StudyA3</span>
+            </h3>
+            <p>
+              International student focus - we help our students achieve their
+              future dreams. With strong interpersonal, negotiating and
+              presentation skills, I am building our existing and new agent
+              relationships.
+            </p>
+          </div>
+
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              NOV 2020 - PRESENT
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Diploma of Social Media Marketing Trainer -{" "}
+              <span className=" font-light">Stanley College</span>
+            </h3>
+            <p>
+              Initially consulting and marketing with potential clients for
+              various service needs from all around the world. Online. Preparing
+              and editing all visa-related submissions and applications (e.g.,
+              visa applications (188, 482, 190, 500), skills assessments,
+              nomination applications, etc).
+            </p>
+          </div>
+
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              FEB 2019 - APR 2019
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Senior Education Consultant-{" "}
+              <span className=" font-light">
+                ANZ Education and Visa Service
+              </span>
+            </h3>
+            <p>
+              Providing education counseling for international students to
+              ensure the most appropriate course selection according to their
+              need and future career.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Experience;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import Hero from "../../components/Hero";
 import AboutMe from "../../components/AboutMe";
 import Services from "../../components/Services";
+import Experience from "../../components/Experience";
 export default function Home() {
   return (
     <main>
       <Hero />
       <AboutMe />
       <Services />
+      <Experience />
     </main>
   );
 }
