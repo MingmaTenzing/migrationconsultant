ab2634e3984a95e0f4665ae54271100bc9bc1326
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-13 14:13:46 +0800

    experience section complete

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,9 +1,9 @@
 type Props = {};
 function Experience({}: Props) {
   return (
-    <div className=" p-4">
-      <div className="  ">
-        <h2 className="  mb-6 text-2xl font-semibold">Experiences</h2>
+    <div className="space-y-10 lg:space-y-0 lg:flex lg:space-x-8 lg:w-[1200px] lg:items-start md:w-2/3 m-auto p-4">
+      <div className="  lg:w-1/2  ">
+        <h2 className="  mb-6 text-2xl md:text-3xl font-semibold">Experiences</h2>
 
         <div className=" space-y-8">
           <div className=" space-y-5">
@@ -52,6 +52,73 @@ function Experience({}: Props) {
               and editing all visa-related submissions and applications (e.g.,
               visa applications (188, 482, 190, 500), skills assessments,
               nomination applications, etc).
+            </p>
+          </div>
+
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              FEB 2019 - APR 2019
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Senior Education Consultant-{" "}
+              <span className=" font-light">
+                ANZ Education and Visa Service
+              </span>
+            </h3>
+            <p>
+              Providing education counseling for international students to
+              ensure the most appropriate course selection according to their
+              need and future career.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/**EDUCATION SECTION */}
+      <div className=" lg:w-1/2  ">
+        <h2 className="  mb-6 text-2xl md:text-3xl font-semibold">Education</h2>
+
+        <div className=" space-y-8">
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              FEB 2022
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Graduate Diploma in Migration Law ad Practice-{" "}
+              <span className=" font-light">
+                University of Technology Sydney
+              </span>
+            </h3>
+            <p>
+              Activities and societies: Migration agent. Immigration to
+              Australia Working visas Student visas
+            </p>
+          </div>
+
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              2009
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Business Management & Marketing-{" "}
+              <span className=" font-light">Universidad de La Sabana</span>
+            </h3>
+            <p>
+              Strategic Manager, Business, Management, Marketing and Related
+              Support Services
+            </p>
+          </div>
+
+          <div className=" space-y-5">
+            <span className=" p-2 border-blue-400 text-sm font-semibold border-2 text-blue-400    uppercase">
+              2004
+            </span>
+            <h3 className="  text-xl font-semibold">
+              Communication and Media -{" "}
+              <span className=" font-light">Universidad Sergio Arboleda</span>
+            </h3>
+            <p>
+             Social Communicator and Journalist, Communication and Media Studiesh
             </p>
           </div>
 
