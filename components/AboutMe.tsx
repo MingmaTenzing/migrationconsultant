d1479451a67f1dd441945c50f57efcc78d281550
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-14 09:27:51 +0800

    about me section changed with image changes

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,18 +2,17 @@ type Props = {};
 function AboutMe({}: Props) {
   return (
     <div className="  py-20 p-8 bg-[#202020] text-white">
-        <div className=" lg:w-[1200px] m-auto flex flex-col  items-center space-y-4">
-
-      <h3 className="  text-bluestyle  ">About Me</h3>
-      <h2 className=" text-3xl md:text-4xl">Welcome To My Site</h2>
-      <p className=" lg:w-[700px] md:text-base text-sm text-center opacity-60 font-light">
-        I am a website designer from London, with a strong focus in UI/UX
-        design. I love to get new experiences and always learn from my
-        surroundings. I've done more than 50 projects. You can check it through
-        portfolio section on this website. I looking forward to any
-        opportunities and challenges.
-      </p>
-        </div>
+      <div className=" lg:w-[1200px] m-auto flex flex-col  items-center space-y-4">
+        <h3 className="  text-bluestyle  ">...</h3>
+        <h2 className=" text-3xl md:text-4xl text-center">Why Choose Carlos Charlie Castro For Your Migration Solution? </h2>
+        <p className=" lg:w-[700px] md:text-base text-sm text-center opacity-60 font-light">
+        Carlos Charlie Castro is a highly regarded migration lawyer specializing in
+          Latin American migration in Perth. As one of the top professionals in
+          this field, I provide personalized and effective solutions for student
+          visas, skilled migration, and family sponsorship. Let me guide you
+          towards a successful migration journey in Australia.
+        </p>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
       <Hero />
       <AboutMe />
       <Services />
-      <Experience />
     </main>
   );
 }
